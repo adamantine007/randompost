475a94086633f87b26cfb84c359aa22efd1ab868
Sergey Prishchepa <adamantine007@gmail.com>
2015-05-16 02:05:57 +0300

    Cancelling last commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,6 +31,8 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
+        dd('check autoupload after commit/push request');
+
 		$books_tmp = \Auth::user()->books()->lists('name', 'id');
 
         $books['0'] = 'Public';
