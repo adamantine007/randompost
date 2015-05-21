12461e453bb022fd2b83641d81b7909e565892f6
Sergey Prishchepa <adamantine007@gmail.com>
2015-05-21 23:24:26 +0300

    add link to 'books/index' in 'books/show' page

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -4,7 +4,10 @@
 
     <div class="row">
         <div class="col-lg-11 col-md-11 col-sm-11 col-xs-10">
-            <h2 class="h2-head">{{ $book->name }}</h2>
+            <h2 class="h2-head">
+                <a href="{{ action('BookController@index') }}">Books</a> /
+                {{ $book->name }}
+            </h2>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-1 col-xs-2 iconInRowInHeader" style="padding-left: 7px;; padding-right: 0">
             <a href="/articles/create?book_id={{ $book->id }}"><i class="fa fa-plus-square-o fa-3x"></i></a>
